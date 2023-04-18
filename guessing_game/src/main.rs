aa2b417425b936eac4f16de59bbb254b02e4bdd1
Pavel Pazderník <pazdernik.pavel@email.cz>
2023-04-18 11:51:47 +0200

    Add guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,47 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..100);
+
+    println!("Secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess = guess.trim();
+
+        if guess == "quit" {
+            println!("You quit!");
+            break;
+        }
+
+        let guess: u32 = match guess.parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid input or type 'quit' to quit.");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
